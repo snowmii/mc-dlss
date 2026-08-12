0c4e779cc2bf1adf1ebf442fabe99314f56e57be
miuki <56622373+snowmii@users.noreply.github.com>
2026-08-12 17:08:45 +0800

    fix: jitter the world projection only

--- src/main/java/me/snowmii/dlss/mixin/GameRendererProjectionJitterMixin.java
+++ src/main/java/me/snowmii/dlss/mixin/GameRendererProjectionJitterMixin.java
@@ -59,7 +59,8 @@ public class GameRendererProjectionJitterMixin {
 		method = "renderLevel",
 		at = @At(
 			value = "INVOKE",
-			target = "Lnet/minecraft/client/renderer/ProjectionMatrixBuffer;getBuffer(Lorg/joml/Matrix4f;)Lcom/mojang/blaze3d/buffers/GpuBufferSlice;"
+			target = "Lnet/minecraft/client/renderer/ProjectionMatrixBuffer;getBuffer(Lorg/joml/Matrix4f;)Lcom/mojang/blaze3d/buffers/GpuBufferSlice;",
+			ordinal = 0
 		)
 	)
 	private GpuBufferSlice mcDlssJitterWorldProjection(
